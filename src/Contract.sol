be259977878444b727ee4c55d2f0aeb392fe65c6
Sk0g3n <skogen27@protonmail.com>
2022-07-09 19:44:34 +0200

    Packing booleans

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Contract {
+    function setBoolean(
+        uint256 _packedBools,
+        uint256 _boolNumber,
+        bool _value
+    ) public view returns(uint256) {
+        if (_value)
+            return _packedBools | uint256(1) << _boolNumber;
+        else
+            return _packedBools & ~(uint256(1) << _boolNumber);
+    }
+
+    function getBoolean(uint256 _packedBools, uint256 _boolNumber)
+        public view returns(bool)
+    {
+        uint256 flag = (_packedBools >> _boolNumber) & uint256(1);
+        return (flag == 1 ? true : false);
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import  "../src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract cntr;
+    uint256 booler;
+    function setUp() public {
+        cntr = new Contract();
+    }
 
     function testExample() public {
-        assertTrue(true);
+        booler = cntr.setBoolean(booler, 89, true);
+        assertEq(cntr.getBoolean(booler, 89), true);
+    }
+
+    function testDoubleSet() public {
+        booler = cntr.setBoolean(booler, 88, true);
+        booler = cntr.setBoolean(booler, 89, true);
+        assertEq(cntr.getBoolean(booler, 89), true);
+        assertEq(cntr.getBoolean(booler, 88), true);
+        assertEq(cntr.getBoolean(booler, 87), false);
+    }
+
+    function testDelete() public {
+        booler = cntr.setBoolean(booler, 88, true);
+        assertEq(cntr.getBoolean(booler, 88), true);
+        booler = cntr.setBoolean(booler, 88, false);
+        assertEq(cntr.getBoolean(booler, 88), false);
     }
 }
